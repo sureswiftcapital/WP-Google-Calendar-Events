79d902123e575e02a0d59b200db6fc60e73e2714
Nick Young <mycorpweb@gmail.com>
2014-07-06 13:24:33 -0600

    Added function to also clear cache when the feed is saved/updated to prevent possible problems with caching

--- google-calendar-events/includes/gce-feed-cpt.php
+++ google-calendar-events/includes/gce-feed-cpt.php
@@ -115,6 +115,16 @@ add_action( 'save_post', 'gce_save_meta' );
 
 
 /**
+ * Delete transient on post save so that the cache gets cleared on any settings changes
+ * 
+ * @since 2.0.0
+ */
+function gce_clear_cache_on_save( $post_id ) {
+	gce_clear_cache( $post_id );
+}
+add_action( 'save_post', 'gce_clear_cache_on_save' );
+
+/**
  * Add column headers to our "All Feeds" CPT page
  * 
  * @since 2.0.0
