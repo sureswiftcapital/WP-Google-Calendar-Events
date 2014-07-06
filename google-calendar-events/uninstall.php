b6877b85ff8a07347e5a880ab9c4dca6fd0a98c9
Nick Young <mycorpweb@gmail.com>
2014-07-06 15:12:42 -0600

    Uninstall actions

--- google-calendar-events/uninstall.php
+++ google-calendar-events/uninstall.php
@@ -15,3 +15,29 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 // TODO uninstall actions
+
+// Remove CPTs
+$feeds = get_posts( array( 
+	'post_type' => 'gce_feed'
+));
+
+foreach( $feeds as $f ) {
+	wp_delete_post( $f->ID, true );
+}
+
+// Remove all post meta
+delete_post_meta_by_key( 'gce_feed_url' );
+delete_post_meta_by_key( 'gce_retrieve_from' );
+delete_post_meta_by_key( 'gce_retrieve_until' );
+delete_post_meta_by_key( 'gce_retrieve_max' );
+delete_post_meta_by_key( 'gce_date_format' );
+delete_post_meta_by_key( 'gce_time_format' );
+delete_post_meta_by_key( 'gce_cache' );
+delete_post_meta_by_key( 'gce_multi_day_events' );
+delete_post_meta_by_key( 'gce_display_mode' );
+delete_post_meta_by_key( 'gce_custom_from' );
+delete_post_meta_by_key( 'gce_custom_until' );
+
+// Remove options
+delete_option( 'gce_upgrade_has_run' );
+delete_option( 'gce_version' );
