2687364e602ddf39080caf45b32f56bf704346a0
Nick Young <mycorpweb@gmail.com>
2014-08-21 21:24:25 -0600

    Added i18n to strings

--- google-calendar-events/views/admin/display-options-meta.php
+++ google-calendar-events/views/admin/display-options-meta.php
@@ -34,40 +34,45 @@
 
 <div id="gce-display-options-wrap">
 	<div class="gce-meta-control">
-		<p><input type="checkbox" name="gce_display_simple" value="1" <?php checked( $gce_display_simple, '1' ); ?> />Use the simple display options below instead of the Event Builder</p>
+		<p>
+			<input type="checkbox" name="gce_display_simple" value="1" <?php checked( $gce_display_simple, '1' ); ?> />
+			<?php _e( 'Use the simple display options below instead of the Event Builder', 'gce' ); ?>
+		</p>
 	</div>
 	
 	<div class="gce-meta-control">
-		<label>Start date / time display</label>
-		<span class="description">Select how to display the start date / time.</span>
+		<label><?php _e( 'Start date / time display', 'gce' ); ?></label>
+		<span class="description"><?php _e( 'Select how to display the start date / time.', 'gce' ); ?></span>
 		<select name="gce_display_start">
-			<option value="none" <?php selected( $gce_display_start, 'none', true ); ?>>None</option>
-			<option value="time" <?php selected( $gce_display_start, 'time', true ); ?>>Start time</option>
-			<option value="date" <?php selected( $gce_display_start, 'date', true ); ?>>Start date</option>
-			<option value="time-date" <?php selected( $gce_display_start, 'time-date', true ); ?>>Start time and date</option>
-			<option value="date-time" <?php selected( $gce_display_start, 'date-time', true ); ?>>Start date and time</option>
+			<option value="none" <?php selected( $gce_display_start, 'none', true ); ?>><?php _e( 'None', 'gce' ); ?></option>
+			<option value="time" <?php selected( $gce_display_start, 'time', true ); ?>><?php _e( 'Start time', 'gce' ); ?></option>
+			<option value="date" <?php selected( $gce_display_start, 'date', true ); ?>><?php _e( 'Start date', 'gce' ); ?></option>
+			<option value="time-date" <?php selected( $gce_display_start, 'time-date', true ); ?>><?php _e( 'Start time and date', 'gce' ); ?></option>
+			<option value="date-time" <?php selected( $gce_display_start, 'date-time', true ); ?>><?php _e( 'Start date and time', 'gce' ); ?></option>
 		</select>
-		<span class="description">Text to display before the start time.</span>
+		<span class="description"><?php _e( 'Text to display before the start time.', 'gce' ); ?></span>
 		<input type="text" name="gce_display_start_text" value="<?php echo $gce_display_start_text; ?>" />
 	</div>
 
 	<div class="gce-meta-control">
-		<label>End time/date display</label>
-		<span class="description">Select how to display the end date / time.</span>
+		<label><?php _e( 'End time/date display', 'gce' ); ?></label>
+		<span class="description"><?php _e( 'Select how to display the end date / time.', 'gce' ); ?></span>
 		<select name="gce_display_end">
-			<option value="none" <?php selected( $gce_display_end, 'none', true ); ?>>None</option>
-			<option value="time" <?php selected( $gce_display_end, 'time', true ); ?>>End time</option>
-			<option value="date" <?php selected( $gce_display_end, 'date', true ); ?>>End date</option>
-			<option value="time-date" <?php selected( $gce_display_end, 'time-date', true ); ?>>End time and date</option>
-			<option value="date-time" <?php selected( $gce_display_end, 'date-time', true ); ?>>End date and time</option>
+			<option value="none" <?php selected( $gce_display_end, 'none', true ); ?>><?php _e( 'None', 'gce' ); ?></option>
+			<option value="time" <?php selected( $gce_display_end, 'time', true ); ?>><?php _e( 'End time', 'gce' ); ?></option>
+			<option value="date" <?php selected( $gce_display_end, 'date', true ); ?>><?php _e( 'End date', 'gce' ); ?></option>
+			<option value="time-date" <?php selected( $gce_display_end, 'time-date', true ); ?>><?php _e( 'End time and date', 'gce' ); ?></option>
+			<option value="date-time" <?php selected( $gce_display_end, 'date-time', true ); ?>><?php _e( 'End date and time', 'gce' ); ?></option>
 		</select>
-		<span class="description">Text to display before the end time.</span>
+		<span class="description"><?php _e( 'Text to display before the end time.', 'gce' ); ?></span>
 		<input type="text" name="gce_display_end_text" value="<?php echo $gce_display_end_text; ?>" />
 	</div>
 
 	<div class="gce-meta-control">
-		<label>Separator</label>
-		<span class="description">If you have chosen to display both the time and date above, enter the text / characters to display between the time and date here (including any spaces).</span>
+		<label><?php _e( 'Separator', 'gce' ); ?></label>
+		<span class="description">
+			<?php _e( 'If you have chosen to display both the time and date above, enter the text / characters to display between the time and date here (including any spaces).' , 'gce' ); ?>
+		</span>
 		<input type="text" name="gce_display_separator" value="<?php echo $gce_display_separator; ?>" />
 	</div>
 
@@ -79,19 +84,28 @@
 	</div>
 
 	<div class="gce-meta-control">
-		<label>Description</label>
-		<p><input type="checkbox" name="gce_display_description" value="1" <?php checked( $gce_display_description, '1' ); ?> />Show the description of events? (URLs in the description will be made into links).</p>
-		<span class="description">Text to display before the description.</span>
+		<label><?php _e( 'Description', 'gce' ); ?></label>
+		<p>
+			<input type="checkbox" name="gce_display_description" value="1" <?php checked( $gce_display_description, '1' ); ?> />
+			<?php _e( 'Show the description of events? (URLs in the description will be made into links).', 'gce' ); ?>
+		</p>
+		<span class="description"><?php _e( 'Text to display before the description.', 'gce' ); ?></span>
 		<input type="text" name="gce_display_description_text" value="<?php echo $gce_display_description_text; ?>" />
-		<span class="description">Maximum number of words to show from description. Leave blank for no limit.</span>
+		<span class="description"><?php _e( 'Maximum number of words to show from description. Leave blank for no limit.', 'gce' ); ?></span>
 		<input type="text" name="gce_display_description_max" value="<?php echo $gce_display_description_max; ?>" />
 	</div>
 
 	<div class="gce-meta-control">
-		<label>Event Link</label>
-		<p><input type="checkbox" name="gce_display_link" value="1" <?php checked( $gce_display_link, '1' ); ?> />Show a link to the Google Calendar page for an event?</p>
-		<p><input type="checkbox" name="gce_display_link_tab" value="1" <?php checked( $gce_display_link_tab, '1' ); ?> />Links open in a new window / tab?</p>
-		<span class="description">The link text to be displayed.</span>
+		<label><?php _e( 'Event Link', 'gce' ); ?></label>
+		<p>
+			<input type="checkbox" name="gce_display_link" value="1" <?php checked( $gce_display_link, '1' ); ?> />
+			<?php _e( 'Show a link to the Google Calendar page for an event?', 'gce' ); ?>
+		</p>
+		<p>
+			<input type="checkbox" name="gce_display_link_tab" value="1" <?php checked( $gce_display_link_tab, '1' ); ?> />
+			<?php _e( 'Links open in a new window / tab?', 'gce' ); ?>
+		</p>
+		<span class="description"><?php _e( 'The link text to be displayed.', 'gce' ); ?></span>
 		<input type="text" name="gce_display_link_text" value="<?php echo $gce_display_link_text; ?>" />
 	</div>
 </div>
