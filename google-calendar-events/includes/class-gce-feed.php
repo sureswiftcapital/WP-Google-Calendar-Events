fdc74682ffbaa7ad67e9d95f8b4b4331917746a6
Nick Young <mycorpweb@gmail.com>
2014-09-19 15:39:00 -0600

    Added gmt_offset back to the start

--- google-calendar-events/includes/class-gce-feed.php
+++ google-calendar-events/includes/class-gce-feed.php
@@ -96,10 +96,10 @@ class GCE_Feed {
 		//Add the default parameters to the querystring (retrieving JSON, not XML)
 		$query = '?alt=json&sortorder=ascending&orderby=starttime';
 
-		//$gmt_offset = get_option( 'gmt_offset' ) * 3600;
+		$gmt_offset = get_option( 'gmt_offset' ) * 3600;
 
 		//Append the feed specific parameters to the querystring
-		$query .= '&start-min=' . date( 'Y-m-d\TH:i:s', mktime( 0, 0, 0, date( 'm' ), 1, date( 'Y' ) ) );
+		$query .= '&start-min=' . date( 'Y-m-d\TH:i:s', mktime( 0, 0, 0, date( 'm' ), 1, date( 'Y' ) ) - $gmt_offset );
 		
 		if ( ! empty( $this->search_query ) ) {
 			$query .= '&q=' . rawurlencode( $this->search_query );
