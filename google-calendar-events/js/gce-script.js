881b7f06b0e7372b0a671003771993002656ebd8
Nick Young <mycorpweb@gmail.com>
2014-10-05 04:08:49 -0600

    Added jquery wrapper

--- google-calendar-events/js/gce-script.js
+++ google-calendar-events/js/gce-script.js
@@ -7,86 +7,90 @@
  * @license   GPL-2.0+
  * @copyright 2014 Phil Derksen
  */
+(function($) {
+	'use strict';
 
-jQuery(document).ready(function($){
-	gce_tooltips('.gce-has-events');
-	
-	$('body').on( 'click', '#' + gce_grid.target_element + ' .gce-change-month', function() {
-		//Extract month and year
-		var month_year = $(this).attr('name').split('-', 2);
-		var paging = jQuery(this).attr('data-gce-grid-paging');
-		
-		//Add loading text to table caption
-		$('#' + gce_grid.target_element + ' caption').html('Loading...');
-		//Send AJAX request
-		$.get(gce.ajaxurl,{
-			action:'gce_ajax',
-			gce_type: gce_grid.type,
-			gce_feed_ids: gce_grid.feed_ids,
-			gce_title_text: gce_grid.title_text,
-			gce_widget_id: gce_grid.target_element,
-			gce_month: month_year[0],
-			gce_year: month_year[1],
-			gce_paging: paging
-		}, function(data){
-			//Replace existing data with returned AJAX data
-			if(gce_grid.type == 'widget'){
-				$('#' + gce_grid.target_element).html(data);
-			}else{
-				$('#' + gce_grid.target_element).replaceWith(data);
-			}
-			gce_tooltips('#' + gce_grid.target_element + ' .gce-has-events');
-		});
-	});
+	$(function() {
 
-	$('.gce-page-list').on( 'click', '.gce-change-month-list', function(e) {
-		
-		e.preventDefault();
-		
-		var element = $(this);
-		
-		var start = $(this).parent().parent().data('gce-start');
-		var grouped = $(this).parent().parent().data('gce-grouped');
-		var title_text = $(this).parent().parent().data('gce-title');
-		var feed_ids = $(this).parent().parent().data( 'gce-feeds');
-		var sort = $(this).parent().parent().data('gce-sort');
-		var paging = $(this).parent().parent().data('gce-paging');
-		var paging_interval = $(this).parent().parent().data('gce-paging-interval');
-		var paging_direction = $(this).data('gce-paging-direction');
-		var start_offset = $(this).parent().parent().data('gce-start-offset');
-		var paging_type = $(this).data('gce-paging-type');
-		
-		//Add loading text to table caption
-		$(this).parent().parent().find('.gce-month-title').html('Loading...');
-		
-		//Send AJAX request
-		jQuery.get(gce.ajaxurl,{
-			action:'gce_ajax_list',
-			gce_feed_ids:feed_ids,
-			gce_title_text:title_text,
-			gce_start: start,
-			gce_grouped: grouped,
-			gce_sort: sort,
-			gce_paging: paging,
-			gce_paging_interval: paging_interval,
-			gce_paging_direction: paging_direction,
-			gce_start_offset: start_offset,
-			gce_paging_type: paging_type
-		}, function(data){
-			element.closest('.gce-page-list').html(data);
-		});
-	});
-	
-	function gce_tooltips(target_items) {
-		$(target_items).each(function(){
-			//Add qtip to all target items
-			$(this).qtip({
-				content: $(this).children('.gce-event-info'),
-				position: { corner: { target: 'center', tooltip: 'bottomLeft' }, adjust: { screen: true } },
-				hide: { fixed: true, delay: 100, effect: { length: 0 } },
-				show: { solo: true, delay: 0, effect: { length: 0 } },
-				style: { padding: "0", classes: { tooltip: 'gce-qtip', tip: 'gce-qtip-tip', title: 'gce-qtip-title', content: 'gce-qtip-content', active: 'gce-qtip-active' }, border: { width: 0 } }
+		gce_tooltips('.gce-has-events');
+
+		$('body').on( 'click', '#' + gce_grid.target_element + ' .gce-change-month', function() {
+			//Extract month and year
+			var month_year = $(this).attr('name').split('-', 2);
+			var paging = jQuery(this).attr('data-gce-grid-paging');
+
+			//Add loading text to table caption
+			$('#' + gce_grid.target_element + ' caption').html('Loading...');
+			//Send AJAX request
+			$.get(gce.ajaxurl,{
+				action:'gce_ajax',
+				gce_type: gce_grid.type,
+				gce_feed_ids: gce_grid.feed_ids,
+				gce_title_text: gce_grid.title_text,
+				gce_widget_id: gce_grid.target_element,
+				gce_month: month_year[0],
+				gce_year: month_year[1],
+				gce_paging: paging
+			}, function(data){
+				//Replace existing data with returned AJAX data
+				if(gce_grid.type == 'widget'){
+					$('#' + gce_grid.target_element).html(data);
+				}else{
+					$('#' + gce_grid.target_element).replaceWith(data);
+				}
+				gce_tooltips('#' + gce_grid.target_element + ' .gce-has-events');
 			});
 		});
-	}
-});
+
+		$('.gce-page-list').on( 'click', '.gce-change-month-list', function(e) {
+
+			e.preventDefault();
+
+			var element = $(this);
+
+			var start = $(this).parent().parent().data('gce-start');
+			var grouped = $(this).parent().parent().data('gce-grouped');
+			var title_text = $(this).parent().parent().data('gce-title');
+			var feed_ids = $(this).parent().parent().data( 'gce-feeds');
+			var sort = $(this).parent().parent().data('gce-sort');
+			var paging = $(this).parent().parent().data('gce-paging');
+			var paging_interval = $(this).parent().parent().data('gce-paging-interval');
+			var paging_direction = $(this).data('gce-paging-direction');
+			var start_offset = $(this).parent().parent().data('gce-start-offset');
+			var paging_type = $(this).data('gce-paging-type');
+
+			//Add loading text to table caption
+			$(this).parent().parent().find('.gce-month-title').html('Loading...');
+
+			//Send AJAX request
+			jQuery.get(gce.ajaxurl,{
+				action:'gce_ajax_list',
+				gce_feed_ids:feed_ids,
+				gce_title_text:title_text,
+				gce_start: start,
+				gce_grouped: grouped,
+				gce_sort: sort,
+				gce_paging: paging,
+				gce_paging_interval: paging_interval,
+				gce_paging_direction: paging_direction,
+				gce_start_offset: start_offset,
+				gce_paging_type: paging_type
+			}, function(data){
+				element.closest('.gce-page-list').html(data);
+			});
+		});
+
+		function gce_tooltips(target_items) {
+			$(target_items).each(function(){
+				//Add qtip to all target items
+				$(this).qtip({
+					content: $(this).children('.gce-event-info'),
+					position: { corner: { target: 'center', tooltip: 'bottomLeft' }, adjust: { screen: true } },
+					hide: { fixed: true, delay: 100, effect: { length: 0 } },
+					show: { solo: true, delay: 0, effect: { length: 0 } },
+					style: { padding: "0", classes: { tooltip: 'gce-qtip', tip: 'gce-qtip-tip', title: 'gce-qtip-title', content: 'gce-qtip-content', active: 'gce-qtip-active' }, border: { width: 0 } }
+				});
+			});
+		}
+	});
+}(jQuery))
