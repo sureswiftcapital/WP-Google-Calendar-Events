3f50256be9c06efb211ac318ea446041fcdd8734
Nick Young <mycorpweb@gmail.com>
2014-11-16 23:30:18 -0700

    Added API settings to settings page

--- google-calendar-events/includes/register-settings.php
+++ google-calendar-events/includes/register-settings.php
@@ -24,17 +24,23 @@ function gce_register_settings() {
 
 		/* General Settings */
 		'general' => array(
-			'save_settings' => array(
-				'id'   => 'save_settings',
-				'name' => __( 'Save Settings', 'gce' ),
-				'desc' => __( 'Save your settings when uninstalling this plugin. Useful when upgrading or re-installing.', 'gce' ),
-				'type' => 'checkbox'
-			),
 			'auth_empty' => array(
 				'id'   => 'auth_empty',
 				'name' => __( 'Authorization', 'gce' ),
 				'desc' => __( 'Authorize use of the API to read your calendar data.', 'gce' ),
 				'type' => 'auth'
+			),
+			'auth_custom' => array(
+				'id'   => 'auth_custom',
+				'name' => __( 'API Settings', 'gce' ),
+				'desc' => __( 'This section allows you to enter your own Google API Settings. It is not required, but is recommended.', 'gce' ),
+				'type' => 'auth_custom'
+			),
+			'save_settings' => array(
+				'id'   => 'save_settings',
+				'name' => __( 'Save Settings', 'gce' ),
+				'desc' => __( 'Save your settings when uninstalling this plugin. Useful when upgrading or re-installing.', 'gce' ),
+				'type' => 'checkbox'
 			)
 		)
 	);
@@ -102,55 +108,38 @@ function gce_auth_callback( $args ) {
 		global $gce_options;
 		
 		if( ! isset( $gce_options['auth_token'] ) ) {
-		
-			//$auth_code   = '';
 			$request_uri = GCal::request_access();
 
 			$html = 'Auth Code: <input type="text" class="auth_code" /><br><br>';
-
 			$html .= '<a href="#" id="gce-auth" class="button-secondary">Authenticate</a>';
-
-
 			$html .= '<a href="' . $request_uri . '" target="_blank" class="button-secondary">Get Access Code</a>';
+			
 		} else {
 			$html = '<a href="#" id="gce-clear-auth" class="button-secondary">Clear Authentication</a>';
-			
-			//$html .= '<input type="hidden" name="gce_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $gce_options[$args['id']] ) . '" />';
-			
-			//echo $gce_options['auth_token']. '<br>';
-			
-			/*GCal::set_token( $gce_options['auth_token'] );
-			
-			$client = GCal::get_client();
-						
-			$calendar_id = 'umsb0ekhivs1a2ubtq6vlqvcjk@group.calendar.google.com';
-
-			$service = new Google_Service_Calendar( $client );
-
-			$events = $service->events->listEvents( $calendar_id );
-
-			$html .= '<ul>';
-
-			while( true ) {
-				foreach( $events->getItems() as $event ) {
-					$html .= '<li>' . $event->getSummary() . '</li>';
-				}
-
-				$pageToken = $events->getNextPageToken();
-
-				if( $pageToken ) {
-					$optParams = array( 'pageToken' => $pageToken );
-					$events = $service->events->listEvents( 'primary', $optParams );
-				} else {
-					break;
-				}
-			}
-
-			$html .= '</ul>';*/
-
 		}
 		
 		echo $html;
+}
+
+function gce_auth_custom_callback( $args ) {
+	// SHow the description first then the form after
+	echo $args['desc'] . '<br>';
+	
+	$html  = '<table>';
+	$html .= '<tr>';
+	$html .= '<td><label>' . __( 'Client ID', 'gce' ) . '</label></td>';
+	$html .= '<td><input type="text" id="gce_settings_' . $args['section'] . '[' . $args['id'] . '_client]" name="gce_settings_' . $args['section'] . '[' . $args['id'] . '_client]" value="" /></td>';
+	$html .= '</tr>';
+	
+	$html .= '<tr>';
+	$html .= '<td><label>' . __( 'Client Secret' , 'gce' ) . '</label></td>';
+	$html .= '<td><input type="text" id="gce_settings_' . $args['section'] . '[' . $args['id'] . '_secret]" name="gce_settings_' . $args['section'] . '[' . $args['id'] . '_secret]" value="" /></td>';
+	$html .= '</tr>';
+	$html .= '</table>';
+	
+	$html .= '<p class="description">' . __( 'Visit <a href="#">this page</a> to see how to get your API credentials', 'gce' ) . '</p>';
+	
+	echo $html;
 }
 
 /*
