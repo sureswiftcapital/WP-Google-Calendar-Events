fc8e82d0053d966fd06a0e9631dfe4c1ea59fd5b
Phil Derksen <pderksen@gmail.com>
2014-11-18 21:25:35 -0800

    Admin activation notice message updated.

--- google-calendar-events/views/admin/api-settings-notice.php
+++ google-calendar-events/views/admin/api-settings-notice.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <style>
-	#gce-install-notice .button-primary,
+	/* #gce-install-notice .button-primary, */
 	#gce-install-notice .button-secondary {
 		margin-left: 15px;
 	}
@@ -24,9 +24,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div id="gce-install-notice" class="updated">
 	<p>
-		<?php echo 'TODO: Content'; ?>
-		
-		<a href="<?php echo admin_url( 'edit.php?post_type=gce_feed&page=google-calendar-events_general_settings' ); ?>" class="button-primary"><?php _e( 'Setup your API Settings now', 'gce' ); ?></a>
+		<strong><?php echo __( 'GCal Events Important Update (Nov. 18, 2014):', 'gce' ); ?></strong><br/>
+		<?php echo __( 'GCal Events now uses the Google API version 3, which requires use of a public key.', 'gce' ); ?>
+		<?php echo __( 'By default a shared key is used across all plugin users, which is limited to 500,000 requests per day and 5 requests per second.', 'gce' ); ?>
+		<?php echo __( 'To avoid these limits you can use your own Google API key.', 'gce' ); ?>
+	</p>
+	<p>
+		<a href="<?php echo admin_url( 'edit.php?post_type=gce_feed&page=google-calendar-events_general_settings' ); ?>" class="button-primary"><?php _e( 'Update your API settings now', 'gce' ); ?></a>
 		<a href="<?php echo add_query_arg( 'gce-dismiss-install-nag', 1 ); ?>" class="button-secondary"><?php _e( 'Hide this', 'gce' ); ?></a>
 	</p>
 </div>
