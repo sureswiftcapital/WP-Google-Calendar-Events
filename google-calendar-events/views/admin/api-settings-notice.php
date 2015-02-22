023e639657e4185b71d3e48c8ec3c6ed21ee40a9
Phil Derksen <pderksen@gmail.com>
2015-02-22 13:37:57 -0800

    Install notice update.

--- google-calendar-events/views/admin/api-settings-notice.php
+++ google-calendar-events/views/admin/api-settings-notice.php
@@ -24,9 +24,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="gce-install-notice" class="updated">
 	<p>
 		<strong><?php _e( 'GCal API Key Notice', 'gce' ); ?></strong><br/>
-		<?php _e( 'GCal Events uses the Google Calendar API version 3, which requires the use of a public key.', 'gce' ); ?>
-		<?php _e( 'By default this plugin uses a public shared key is used across all plugin users. This key is limited to 500,000 requests per day and 5 requests per second.', 'gce' ); ?>
-		<?php _e( 'To avoid these limits you can use your own Google API key.', 'gce' ); ?>
+		<?php _e( 'GCal Events uses the Google Calendar API version 3. By default this plugin uses a public shared key across all plugin users.', 'gce' ); ?>
+		<br/><br/>
+		<?php _e( 'This key is limited to 500,000 requests per day and 5 requests per second. To avoid running into any potential limits you can use your own Google API key.', 'gce' ); ?>
 	</p>
 	<p>
 		<a href="<?php echo admin_url( 'edit.php?post_type=gce_feed&page=google-calendar-events_general_settings' ); ?>" class="button-primary"><?php _e( 'Enter your GCal API key', 'gce' ); ?></a>
