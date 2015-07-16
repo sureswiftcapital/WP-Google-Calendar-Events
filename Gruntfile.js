c3b9672f0f7f84d3ee80ea94bcb0fa72ce89fa8c
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-07-16 18:23:26 +0200

    new gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function( grunt ) {
+
+	var pkg = grunt.file.readJSON( 'package.json' ),
+		// version = 'vX.Y.Z'
+		version = pkg.version,
+		// semver = 'X.Y.Z'
+		semver = version.substring( 1, version.length ),
+		// Files to include in a release
+		distFiles =  [
+			'google-calendar-events/**',
+			'!google-calendar-events/assets/**'
+		];
+
+	// Print current version number converted to semantic versioning
+	console.log( pkg.version + ' => ' + semver );
+
+	// Project configuration
+	grunt.initConfig( {
+
+		pkg: pkg,
+		semver : semver,
+
+		clean: {
+			main: [ 'build' ]
+		},
+
+		copy: {
+			main: {
+				expand: true,
+				src: distFiles,
+				dest: 'build/google-calendar-events'
+			}
+		},
+
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: './build/google-calendar-events-<%= semver %>.zip'
+				},
+				expand: true,
+				src: distFiles,
+				dest: '/google-calendar-events'
+			}
+		},
+
+		wp_deploy: {
+			deploy: {
+				options: {
+					plugin_slug: 'google-calendar-events',
+					build_dir: 'build/google-calendar-events',
+					assets_dir: 'google-calendar-events/assets'
+				}
+			}
+		}
+
+	} );
+
+	// Load tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Register tasks
+
+	grunt.registerTask( 'release', ['clean', 'copy', 'compress'] );
+
+	grunt.registerTask( 'deploy', ['release', 'wp_deploy'] );
+
+	grunt.util.linefeed = '\n';
+};
